5099a0a25f26207c892aced9eaa1d2e996a99d1f
Ger-Jan te Dorsthorst <gerjantd@gmail.com>
2019-05-18 12:41:22 +0200

    Update GAV

--- cnam-war/src/main/java/com/example/util/JBPMConstant.java
+++ cnam-war/src/main/java/com/example/util/JBPMConstant.java
@@ -2,7 +2,7 @@ package com.example.util;
 
 public class JBPMConstant {
 
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0";
 	public static final String ARTIFACT = "cnam-kjar";
 	public static final String GROUP = "com.example";
 	public static final String PROCESS_ID = "Evaluation.cnam-simple";
